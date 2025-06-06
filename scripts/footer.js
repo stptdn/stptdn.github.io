a4cc1fcf0442b0d0319b4451b88583c6cce4fc0a
Daniil <xmegatech89@gmail.com>
2025-06-06 16:02:55 +0500

    added header and footer

---
+++ scripts/footer.js
@@ -0,0 +1,5 @@
+const footer = document.createElement("footer");
+const p = document.createElement("p");
+p.innerText = "Батырханов Даниил, ПрИ-101. 2025";
+footer.appendChild(p);
+body.appendChild(footer);
---
+++ scripts/header.js
@@ -0,0 +1,29 @@
+const body = document.querySelector("body");
+
+const links = [
+    { href: "./index.html", text: "Home" },
+    { href: "./blog.html", text: "Blog" },
+    { href: "./about.html", text: "About the site" },
+];
+
+// const header = document.createElement("header");
+// const nav = document.createElement("nav");
+// const ul = document.createElement("ul");
+
+header.classList.add("header");
+nav.classList.add("nav");
+ul.classList.add("nav-list");
+
+links.forEach((link) => {
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+    a.href = link.href;
+    a.textContent = link.text;
+    li.appendChild(a);
+    ul.appendChild(li);
+});
+
+nav.appendChild(ul);
+header.appendChild(nav);
+
+body.insertBefore(header, body.firstChild);
