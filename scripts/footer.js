15cf479e20de3849f38b6b56dd8e576907d1ac1e
Daniil <xmegatech89@gmail.com>
2025-06-07 15:21:58 +0500

    Updates

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,5 +1,9 @@
 const footer = document.createElement("footer");
 const p = document.createElement("p");
+
+footer.classList.add("flex", "items-center", "h-10");
+p.classList.add("ml-8");
+
 p.innerText = "Батырханов Даниил, ПрИ-101. 2025";
 footer.appendChild(p);
 body.appendChild(footer);
--- scripts/header.js
+++ scripts/header.js
@@ -1,26 +1,25 @@
 const body = document.querySelector("body");
 
 const links = [
-    { href: "./index.html", text: "Home" },
-    { href: "./blog.html", text: "Blog" },
-    { href: "./about.html", text: "About the site" },
+  { href: "./index.html", text: "Home" },
+  { href: "./blog.html", text: "Blog" },
+  { href: "./about.html", text: "About the site" },
 ];
 
 const header = document.createElement("header");
 const nav = document.createElement("nav");
 const ul = document.createElement("ul");
 
-header.classList.add("flex", "items-center", "h-12");
-// nav.classList.add("nav");
-ul.classList.add("flex", "gap-8", "mx-8");
+header.classList.add("flex", "items-center", "h-14");
+ul.classList.add("flex", "gap-8", "ml-8");
 
 links.forEach((link) => {
-    const li = document.createElement("li");
-    const a = document.createElement("a");
-    a.href = link.href;
-    a.textContent = link.text;
-    li.appendChild(a);
-    ul.appendChild(li);
+  const li = document.createElement("li");
+  const a = document.createElement("a");
+  a.href = link.href;
+  a.textContent = link.text;
+  li.appendChild(a);
+  ul.appendChild(li);
 });
 
 nav.appendChild(ul);
