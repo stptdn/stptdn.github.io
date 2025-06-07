f52932bf3590514079d035dcf18b1d77dfae30c7
Daniil <xmegatech89@gmail.com>
2025-06-08 00:31:00 +0500

    idk

--- src/scripts/footer.js
+++ src/scripts/footer.js
@@ -1,5 +1,9 @@
 const footer = document.createElement("footer");
 const p = document.createElement("p");
+
+footer.classList.add("flex", "items-center", "h-10", "justify-center");
+p.classList.add("px-6", "font-mono", "text-sm");
+
 p.innerText = "Батырханов Даниил, ПрИ-101. 2025";
 footer.appendChild(p);
 body.appendChild(footer);
--- src/scripts/header.js
+++ src/scripts/header.js
@@ -1,26 +1,39 @@
 const body = document.querySelector("body");
 
 const links = [
-    { href: "./index.html", text: "Home" },
-    { href: "./blog.html", text: "Blog" },
-    { href: "./about.html", text: "About the site" },
+  { href: "./index.html", text: "Home" },
+  { href: "./blog.html", text: "Blog" },
+  { href: "./contacts.html", text: "Contacts" },
+  {
+    href: "https://www.figma.com/design/5DR1gasq5LMBOsSaxtBS2J/%D0%BC%D0%BE%D0%BA%D0%B0%D0%BF-%D1%81%D0%B0%D0%B9%D1%82%D0%B0-%D0%B2%D0%B2%D0%BD?node-id=0-1&t=5L8kWU2CiiEEqolu-1",
+    text: "Mockup",
+  },
 ];
 
 const header = document.createElement("header");
 const nav = document.createElement("nav");
 const ul = document.createElement("ul");
 
-header.classList.add("flex", "items-center", "h-12");
-// nav.classList.add("nav");
-ul.classList.add("flex", "gap-8", "mx-8");
+header.classList.add(
+  "flex",
+  "items-center",
+  "h-14",
+  "fixed",
+
+  "w-full",
+  "prose-a:hover:underline",
+  "justify-center",
+);
+nav.classList.add("w-full", "max-w-3xl", "px-6");
+ul.classList.add("flex", "justify-between", "sm:justify-around");
 
 links.forEach((link) => {
-    const li = document.createElement("li");
-    const a = document.createElement("a");
-    a.href = link.href;
-    a.textContent = link.text;
-    li.appendChild(a);
-    ul.appendChild(li);
+  const li = document.createElement("li");
+  const a = document.createElement("a");
+  a.href = link.href;
+  a.textContent = link.text;
+  li.appendChild(a);
+  ul.appendChild(li);
 });
 
 nav.appendChild(ul);
