495d2926f791fa85d13d6b09d8315b939cfff4fa
Daniil <xmegatech89@gmail.com>
2025-06-07 20:00:23 +0500

    Updates

--- scripts/header.js
+++ scripts/header.js
@@ -3,14 +3,25 @@ const body = document.querySelector("body");
 const links = [
   { href: "./index.html", text: "Home" },
   { href: "./blog.html", text: "Blog" },
-  { href: "./about.html", text: "About the site" },
+  { href: "./about.html", text: "Contacts" },
+  {
+    href: "https://www.figma.com/design/5DR1gasq5LMBOsSaxtBS2J/%D0%BC%D0%BE%D0%BA%D0%B0%D0%BF-%D1%81%D0%B0%D0%B9%D1%82%D0%B0-%D0%B2%D0%B2%D0%BD?node-id=0-1&t=5L8kWU2CiiEEqolu-1",
+    text: "Mockup",
+  },
 ];
 
 const header = document.createElement("header");
 const nav = document.createElement("nav");
 const ul = document.createElement("ul");
 
-header.classList.add("flex", "items-center", "h-14");
+header.classList.add(
+  "flex",
+  "items-center",
+  "h-14",
+  "fixed",
+  "w-full",
+  "prose-a:hover:underline",
+);
 ul.classList.add("flex", "gap-8", "ml-8");
 
 links.forEach((link) => {
